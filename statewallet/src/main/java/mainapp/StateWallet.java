b992a42b7a46dcdb596ef5a821f9e41cf0a80198
Giannis Mpalomenos <t8240084@aueb.gr>
2025-11-08 22:33:22 +0200

    Finished python script and made added code in main to use the script

--- statewallet/src/main/java/mainapp/StateWallet.java
+++ statewallet/src/main/java/mainapp/StateWallet.java
@@ -1,8 +1,28 @@
 package mainapp;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class StateWallet {
     public static void main(String[] args) {
-        
+        try {
+            String scriptPath = "Prog2\\statewallet\\src\\scripts\\pdftocsv.py";
+
+            ProcessBuilder pb = new ProcessBuilder("python", scriptPath);
+            pb.redirectErrorStream(true);
+
+            Process process = pb.start();
+            BufferedReader reader = new BufferedReader(
+            new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("[PYTHON] " + line);
+            }
+            int exitCode = process.waitFor();
+            System.out.println("Python process exited with code: " + exitCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
