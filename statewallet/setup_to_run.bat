037ec2b5e6ade34a64ec3a3b28f9b07704cf64b6
Giannis Mpalomenos <t8240084@aueb.gr>
2025-11-09 20:42:50 +0200

    Added a setup file for python libraries

---
+++ statewallet/setup_to_run.bat
@@ -0,0 +1,31 @@
+@echo off
+title Setup Python
+color 0A
+
+echo ==================================================
+echo     Python scripts setup for StateWallet
+echo ==================================================
+echo.
+
+REM === Cheching if Python is installed ===
+python --version >nul 2>&1
+IF ERRORLEVEL 1 (
+    echo [ERROR] Python is not installed
+    echo Download latest version bellow:
+    echo https://www.python.org/downloads/
+    pause
+    exit /b
+)
+
+echo [INFO] Python was found!
+python --version
+echo.
+
+REM === Downloading essential libraries for Python scripts ===
+echo [INFO] Downloading essential libraries...
+python -m pip install --upgrade pip
+python -m pip install PyMuPDF
+
+echo.
+echo [OK] The Python script is ready to run!
+echo.
