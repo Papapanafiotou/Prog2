9bcc70fab6654121d5bfe5150bc70da93c5e5c8b
Maximos Denaxas <t8240039@aueb.gr>
2025-11-12 10:58:40 +0200

    Change message

--- statewallet/src/main/java/mainapp/Constrains.java
+++ statewallet/src/main/java/mainapp/Constrains.java
@@ -2,9 +2,10 @@ package mainapp;
 
 public class Constrains {
 
+    // Έλεγχος αν ο αριθμός είναι θετικός //
     public static boolean negativeAmount(double amount){
         if (amount < 0) {
-                System.out.println("!! ΣΦΑΛΜΑ: Το ποσό δεν μπορεί να είναι αρνητικό.");
+                System.out.println("ΣΦΑΛΜΑ: Το ποσό δεν μπορεί να είναι αρνητικό.");
                 return true;
             }
         return false;
