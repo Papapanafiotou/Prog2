ef3e71af8b26ba2e5f48cdc34319b5657eadc8e6
Giannis Mpalomenos <t8240084@aueb.gr>
2025-11-24 20:16:35 +0200

    Make seperate class for CSV to PDF

--- statewallet/src/main/java/mainapp/StateWallet.java
+++ statewallet/src/main/java/mainapp/StateWallet.java
@@ -1,10 +1,9 @@
 package mainapp;
 
-import java.nio.file.*;
+
 import java.util.Scanner;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 
 public class StateWallet {
     public static void main(String[] args) {
@@ -17,44 +16,6 @@ public class StateWallet {
         } while(year <2023 || year > 2026);
 
         scanner.close();
-        String path ="statewallet/src/main/sources/budget" + year + ".pdf";
-        String newPath = "statewallet/src/main/sources/budgettouse.pdf";
-        
-        try {
-            
-            Files.move(Paths.get(path), Paths.get(newPath), StandardCopyOption.REPLACE_EXISTING);
-
-            System.out.print("Το αρχείο μετονομάστηκε επιτυχώς σε: ");
-            System.out.println(newPath);
-
-        } catch (Exception e) {
-            System.out.println("Σφάλμα κατά τη μετονομασία: " + e.getMessage());
-        }
-        try {
-            String scriptPath = "statewallet\\src\\scripts\\pdftocsv.py";
-
-            ProcessBuilder pb = new ProcessBuilder("python", scriptPath);
-            pb.redirectErrorStream(true);
-
-            Process process = pb.start();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println("[PYTHON] " + line);
-            }
-            int exitCode = process.waitFor();
-            System.out.println("Python process exited with code: " + exitCode);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            Files.move(Paths.get(newPath), Paths.get(path), StandardCopyOption.REPLACE_EXISTING);
-
-            System.out.print("Το αρχείο μετονομάστηκε ξανά στο αρχικό επιτυχώς σε: ");
-            System.out.println(path);
-
-        } catch (Exception e) {
-            System.out.println("Σφάλμα κατά τη μετονομασία: " + e.getMessage());
-        }
+        Csvtopdf.run(year);
     }
 }
