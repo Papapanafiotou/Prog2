f4a94a35952a7f8ffd33bd11b845e1806b1753fe
marios-voutsas <t8240020@aueb.gr>
2025-11-24 21:08:43 +0200

    Addimports

--- statewallet/src/main/java/mainapp/Charts.java
+++ statewallet/src/main/java/mainapp/Charts.java
@@ -1,5 +1,12 @@
 package mainapp;
 
+import java.sql.*;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+import java.io.File;
+
 public class Charts {
     
 }
