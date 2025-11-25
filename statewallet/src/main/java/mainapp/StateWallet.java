8024af1893213ab385cebf6f2ac1d1d7efd5a82a
AchilleasDamianidis <t8240241@aueb.gr>
2025-11-25 17:02:01 +0200

    Test

--- statewallet/src/main/java/mainapp/StateWallet.java
+++ statewallet/src/main/java/mainapp/StateWallet.java
@@ -5,13 +5,11 @@ public class StateWallet{
 
     public static void main(String[] args) {
         final String DATABASE_URL = "jdbc:sqlite:budget.db";
-        // 1. Δημιουργία του αντικειμένου που διαχειρίζεται το μενού
         PinakesImporter importer = new PinakesImporter(DATABASE_URL);
         importer.importAll();
 
         BudgetMenu menu = new BudgetMenu();
         
-        // 2. Εκκίνηση της εφαρμογής
         menu.start();
     }
 }
