bef00d8623002a68d58ff6806c5e94bc0e820f51
Chriskaraxalios <t8240060@aueb.gr>
2025-11-25 12:36:50 +0200

    Change Main

--- statewallet/src/main/java/mainapp/StateWallet.java
+++ statewallet/src/main/java/mainapp/StateWallet.java
@@ -1,5 +1,6 @@
 package mainapp;
 
+import java.util.Scanner;
 import java.sql.SQLException;
 public class StateWallet {
     public static void main(String[] args) {
@@ -7,6 +8,26 @@ public class StateWallet {
         PinakesImporter importer = new PinakesImporter("jdbc:sqlite:budget.db");
 importer.importAll();  
         System.out.println("Ολα ok!");
+        System.out.println(" --ΜΕΝΟΥ ΕΠΙΛΟΓΩΝ-- ");
+        
+        Scanner scan = new Scanner(System.in);
+        int choice = scan.nextInt();
+        switch (choice) {
+    case 1:
+        budgetChar b1 = new budgetChar();
+        b1.budgetCharacterism();;
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    default:
+        break;
+}
     } catch (Exception e) {
         e.printStackTrace();
     }
