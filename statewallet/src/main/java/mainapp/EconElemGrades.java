5df15f90dc9de43d95e07cd8ead06e9742d8c33a
Chriskaraxalios <t8240060@aueb.gr>
2025-12-01 20:16:47 +0200

    Add economic grade calculation  method

--- statewallet/src/main/java/mainapp/EconElemGrades.java
+++ statewallet/src/main/java/mainapp/EconElemGrades.java
@@ -25,12 +25,21 @@ public class EconElemGrades {
 Το πρωτογενές πλεόνασμα ως ποσοστό του ΑΕΠ δείχνει
 ι πόσα χρήματα περίσσεψαν ή χάθηκαν από τον Κρατικό Προϋπολογισμό,
  χωρίς να υπολογίζονται οι τόκοι που πληρώνει το κράτος για τα δάνεια*/
-     public int getSurplusGrade(double percent) {
+    public int getSurplusGrade(double percent) {
         if (percent > 0.03) return 10;
         else if (percent >= 0.023) return 9;
         else if (percent >= 0.016) return 8;
         else if (percent >= 0.09) return 7;
         else if (percent >= 0.0) return 6;
         else return 5;
-    }   
+    }
+    
+    public double getEconomicGrade(double w1, double w2, double w3,
+        double surplusPercent, double debtPercent, double gdpPercent) {
+            int g1 = getGDPGrowthGrade(gdpPercent);
+            int g2 = getpublicDebtGrade(debtPercent);
+            int g3 = getSurplusGrade(surplusPercent);
+            double grade = w1 *g1 + w2 *g2 + w3 *g3;
+            return grade;
+        }
 }
