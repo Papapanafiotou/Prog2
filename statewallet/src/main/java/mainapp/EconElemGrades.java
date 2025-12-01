948aa0c6d2f3dabf8ed5ca73eefe51673d46b2bc
Chriskaraxalios <t8240060@aueb.gr>
2025-12-01 19:52:13 +0200

    Add primary surplus grading system

--- statewallet/src/main/java/mainapp/EconElemGrades.java
+++ statewallet/src/main/java/mainapp/EconElemGrades.java
@@ -1,5 +1,5 @@
 public class EconElemGrades {
-    //Βαθμός για την μεγένθυση του ΑΕΠ
+//Βαθμός για την μεγένθυση του ΑΕΠ
     public int getGDPGrowthGrade(double percent) {
         if (percent >= 0.022) return 10;
         else if (percent >= 0.015) return 9;
@@ -9,13 +9,26 @@ public class EconElemGrades {
         else return 5;
     }
 
-    //Βαθμός για το ποσοστό του δημοσίου χρέους στο ΑΕΠ
-     public int getpublicDebtGrade(double percent) {
-        if (percent < 141) return 10;
-        else if (percent < 145) return 9;
-        else if (percent < 150) return 8;
-        else if (percent < 154) return 7;
-        else if (percent < 158) return 6;
+//Βαθμός για το δημόσιο χρέος ως ποσοστό του ΑΕΠ
+    public int getpublicDebtGrade(double percent) {
+        if (percent < 141.0) return 10;
+        else if (percent < 145.0) return 9;
+        else if (percent < 150.0) return 8;
+        else if (percent < 154.0) return 7;
+        else if (percent < 158.0) return 6;
         else return 5;
     }
+
+/* Βαθμός για το πρωτογενές πλεόνασμα του κράτους ως ποσοστό του ΑΕΠ
+Το πρωτογενές πλεόνασμα ως ποσοστό του ΑΕΠ δείχνει
+ι πόσα χρήματα περίσσεψαν ή χάθηκαν από τον Κρατικό Προϋπολογισμό,
+ χωρίς να υπολογίζονται οι τόκοι που πληρώνει το κράτος για τα δάνεια*/
+     public int getSurplusGrade(double percent) {
+        if (percent > 0.03) return 10;
+        else if (percent >= 0.023) return 9;
+        else if (percent >= 0.016) return 8;
+        else if (percent >= 0.09) return 7;
+        else if (percent >= 0.0) return 6;
+        else return 5;
+    }   
 }
