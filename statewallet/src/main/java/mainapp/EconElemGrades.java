98f628e91ce6aa4574bf8c78ffde7028287e18d7
Chriskaraxalios <t8240060@aueb.gr>
2025-12-01 19:34:03 +0200

    Add public debt  grading system

--- statewallet/src/main/java/mainapp/EconElemGrades.java
+++ statewallet/src/main/java/mainapp/EconElemGrades.java
@@ -3,9 +3,19 @@ public class EconElemGrades {
     public int getGDPGrowthGrade(double percent) {
         if (percent >= 0.022) return 10;
         else if (percent >= 0.015) return 9;
-        else if (percent >= 0.012) return 9;
-        else if (percent >= 0.009) return 9;
-        else if (percent >= 0.005) return 9;
+        else if (percent >= 0.012) return 8;
+        else if (percent >= 0.009) return 7;
+        else if (percent >= 0.005) return 6;
+        else return 5;
+    }
+
+    //Βαθμός για το ποσοστό του δημοσίου χρέους στο ΑΕΠ
+     public int getpublicDebtGrade(double percent) {
+        if (percent < 141) return 10;
+        else if (percent < 145) return 9;
+        else if (percent < 150) return 8;
+        else if (percent < 154) return 7;
+        else if (percent < 158) return 6;
         else return 5;
     }
 }
