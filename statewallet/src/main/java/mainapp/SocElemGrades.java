f775bc3a836b8977d83f34afe918171690672a6c
Chriskaraxalios <t8240060@aueb.gr>
2025-12-04 22:16:59 +0200

    Change getSocialGrade method

--- statewallet/src/main/java/mainapp/SocElemGrades.java
+++ statewallet/src/main/java/mainapp/SocElemGrades.java
@@ -42,10 +42,23 @@ public class SocElemGrades {
     public double getSocialGrade(double w1, double w2, double w3, double w4,
          double gini, double crimePercent, double mentalHealthPercent,
          double eduHealthExpensesPercent) {
+            GradeChar gradeChar = new GradeChar();
             int g1 = getGINIGrade(gini);
+            System.out.println("O βαθμός για την τιμή του δείκτη ανισοτήτων "
+            + "GINI είναι: " + g1);
+            gradeChar.giniChar(g1); //Χαρακτηρισμός τησ επίδοσης για το στοιχείο
             int g2 = getCrimeGrade(crimePercent);
+            System.out.println("O βαθμός για την μεταβολή της εγκληματικότητας"
+            + "είναι: " + g2);
+            gradeChar.crimeRateChar(g2);
             int g3 = getMentalHealthGrade(mentalHealthPercent);
+             System.out.println("O βαθμός για την εκτίμηση του ποσοστού " 
+            + "ατόμων με προβλήματα ψυχικής υγείας στον πλυθησμό είναι: " 
+                + g3);
             int g4 = getHealthEduGrade(eduHealthExpensesPercent);
+            System.out.println("O βαθμός για τις δαπάνες σε υγεία και "
+            + "παιδεία ως ποσοστό του ΑΕΠ είναι: " + g4);
+            gradeChar.edHealthChar(g4);
             double grade = g1 * w1 + g2 * w2 + g3 * w3 + g4 * w4;
             System.out.println("O βαθμός για το κράτος στον κοινωνικό"
                 + " τομέα με βάση τα στοιχεία είναι " + grade);
