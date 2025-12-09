aca2da19af3e8849648671add774ae39ec4ac676
Chriskaraxalios <t8240060@aueb.gr>
2025-12-09 19:03:21 +0200

    add createccount method

--- statewallet/src/main/java/mainapp/Accounts.java
+++ statewallet/src/main/java/mainapp/Accounts.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class Accounts {
     public void createTable() {
@@ -18,6 +19,18 @@ public class Accounts {
                 System.out.println("Ο πίνακας δημιουργήθηκε!");
             } catch (SQLException e) {
             // Χειρισμός σφαλμάτων JDBC.
+            System.err.println(e.getMessage());
+        }
+    }
+    public void createAccount(String name, String pass) {
+        String url = "jdbc:sqlite:accounts.db";
+        String sql = "INSERT INTO accounts(username, password) VALUES(?, ?)";
+        try (Connection conn = DriverManager.getConnection(url);
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, name);
+                pstmt.setString(2, pass);
+                pstmt.executeUpdate();
+            } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
     }
