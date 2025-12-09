6221cabd6d3aa128506b4cdcdf2df50ae7838733
Chriskaraxalios <t8240060@aueb.gr>
2025-12-09 18:51:19 +0200

    add createable method

--- statewallet/src/main/java/mainapp/Accounts.java
+++ statewallet/src/main/java/mainapp/Accounts.java
@@ -1,3 +1,24 @@
+package mainapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Accounts {
-    
+    public void createTable() {
+        String url = "jdbc:sqlite:accounts.db";
+        String sql = "CREATE TABLE IF NOT EXISTS accounts (" +
+                         "username TEXT NOT NULL," +
+                         "password TEXT NOT NULL" +
+                         ");";
+        try (Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.prepareStatement(sql)) {
+                stmt.execute(sql);
+                System.out.println("Ο πίνακας δημιουργήθηκε!");
+            } catch (SQLException e) {
+            // Χειρισμός σφαλμάτων JDBC.
+            System.err.println(e.getMessage());
+        }
+    }
 }
