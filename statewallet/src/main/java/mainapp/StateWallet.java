7cf3e87d1ff4c5fcdf79c687feb1fe09a6a29a53
marios-voutsas <t8240020@aueb.gr>
2025-12-15 20:11:32 +0200

    add processmethod

--- statewallet/src/main/java/mainapp/StateWallet.java
+++ statewallet/src/main/java/mainapp/StateWallet.java
@@ -1,30 +1,32 @@
 package mainapp;
 
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import java.util.Scanner;
 
 
 
 public class StateWallet {
     
-    @SuppressWarnings("CallToPrintStackTrace")
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
-
-        // 2. Εκκίνηση του Launcher (Το πρώτο παράθυρο)
-        SwingUtilities.invokeLater(() -> {
-            StateWalletLauncher launcher = new StateWalletLauncher();
-            launcher.setVisible(true);
-        });
-    
        
+    
+        try (Scanner scanner = new Scanner(System.in)) { // χρηση try-with για να κλεισει αυτοματα το scanner //
+            int year;
+            
+            do {
+                System.out.print("Δώσε χρονολογία (2023 έως 2026): ");
+                year = scanner.nextInt();
+            } while(year <2023 || year > 2026);
+            final String DATABASE_URL = "jdbc:sqlite:budget.db";
+            PinakesImporter importer = new PinakesImporter(DATABASE_URL);
+            Csvtopdf.run(year);
+            importer.importAll();
+            
+            BudgetMenu budgetmenu = new BudgetMenu();
+            
+            // 2. Εκκίνηση της εφαρμογής
+            budgetmenu.start();
         }
     }
-
+}
 
