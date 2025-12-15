cfe6762eeb9526c7d160c9150c774603bbbd1de5
marios-voutsas <t8240020@aueb.gr>
2025-12-15 14:26:12 +0200

    addbasictools

--- statewallet/src/main/java/mainapp/StateWalletLauncher.java
+++ statewallet/src/main/java/mainapp/StateWalletLauncher.java
@@ -1,7 +1,29 @@
 package mainapp;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import javax.swing.border.EmptyBorder;
 
 public class StateWalletLauncher extends JFrame {
     
+    private final JComboBox<String> yearSelector;
+    private final JButton startButton;
+    private final JLabel statusLabel;
+    private final JProgressBar progressBar;
 }
