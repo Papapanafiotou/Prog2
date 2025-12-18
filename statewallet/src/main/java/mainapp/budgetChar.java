858cf40615f086d679d7ee68b78cc9208566faa4
Chriskaraxalios <t8240060@aueb.gr>
2025-12-18 15:14:36 +0200

    Add budgetChar to main

---
+++ statewallet/src/main/java/mainapp/budgetChar.java
@@ -0,0 +1,16 @@
+package mainapp;
+
+public class budgetChar {
+  public void budgetCharacterism() {
+    GetTotal g1 = new GetTotal();
+    double revenue = g1.getTotalRevenue();
+    double expenses = g1.getTotalExpenses(); 
+        if (revenue > expenses) {
+            System.out.println("Ο προϋπολογισμός είναι πλεονασματικός");
+        } else if (revenue < expenses) {
+            System.out.println("Ο προϋπολογισμός είναι ελλειματικός");
+        } else {
+            System.out.println("Ο προϋπολογισμός είναι ισοσκελισμένος");
+        }
+    }  
+}
