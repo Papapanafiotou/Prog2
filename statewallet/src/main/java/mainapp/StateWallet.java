e457e373f2c5e02a3243470e229408d6776ccfbe
Chriskaraxalios <t8240060@aueb.gr>
2025-12-24 16:54:44 +0200

    Change stateWallet

--- statewallet/src/main/java/mainapp/StateWallet.java
+++ statewallet/src/main/java/mainapp/StateWallet.java
@@ -4,12 +4,16 @@ package mainapp;
 public class StateWallet {
     
     public static void main(String[] args) {
+            Log log = new Log();
+            boolean login = log.logMenu();
+            if (login) {
             DatabaseChooser chooser = new DatabaseChooser();
             String DATABASE_URL = chooser.getURL();
             BudgetMenu budgetmenu = new BudgetMenu(DATABASE_URL);
             
             // 2. Εκκίνηση της εφαρμογής
             budgetmenu.start();
+            }
         }
 }
 
