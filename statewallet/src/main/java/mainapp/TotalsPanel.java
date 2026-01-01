2e3d1846e496c8382c3c3a5ab3d5584a5f144d4f
marios-voutsas <t8240020@aueb.gr>
2026-01-01 15:13:36 +0200

    add imports

---
+++ statewallet/src/main/java/mainapp/TotalsPanel.java
@@ -0,0 +1,10 @@
+package mainapp;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public final class TotalsPanel extends JPanel {
