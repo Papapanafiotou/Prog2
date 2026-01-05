2d76cbe737157486dd9e9e6ef22bbc0778f1216e
Xristina <t8240025@aueb.gr>
2026-01-05 14:36:14 +0200

    add label and dropdown

--- statewallet/src/main/java/mainapp/PercentageUI.java
+++ statewallet/src/main/java/mainapp/PercentageUI.java
@@ -9,3 +9,12 @@ public class PercentageUI extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLayout(null);
+                JLabel label = new JLabel("Επιλέξτε Πίνακα:");
+        label.setBounds(20, 20, 200, 20);
+        add(label);
+
+        JComboBox<String> combo = new JComboBox<>(
+            new String[]{"Έσοδα", "Έξοδα", "Υπουργεία"}
+        );
+        combo.setBounds(20, 50, 200, 25);
+        add(combo);
