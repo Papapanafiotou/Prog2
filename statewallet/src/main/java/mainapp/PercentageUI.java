35cdd08370a45ced3198d11aa5775d0b79bf0cc9
Xristina <t8240025@aueb.gr>
2026-01-05 14:23:54 +0200

    make window

--- statewallet/src/main/java/mainapp/PercentageUI.java
+++ statewallet/src/main/java/mainapp/PercentageUI.java
@@ -1,1 +1,11 @@
 package mainapp;
+import javax.swing.*;
+
+public class PercentageUI extends JFrame {
+
+    public PercentageUI(BudgetManager manager) {
+        setTitle("Υπολογισμός Ποσοστών");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(null);
